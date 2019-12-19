4479f89042686ecbcab2ac006e6a1d8467c21f33
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2019-12-19 12:12:31 +0800

    Prevent seed to create same postcounter with tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-a = PostCounter.new
-a.tags = "default"
-a.limit = 3
-a.save
+if PostCounter.where(tags: "default").blank?
+  a = PostCounter.new
+  a.tags = "default"
+  a.limit = 3
+  a.save
+end
