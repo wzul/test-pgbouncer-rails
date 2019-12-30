27798f2a6915126a3dec7f808b89432cabcaeec8
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2019-12-30 09:34:05 +0800

    Test on_worker_boot

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,9 @@ preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
